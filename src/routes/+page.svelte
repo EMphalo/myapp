280f2ae0f05cf219f5f7aa49196eb6b6d1b7baf5
EMphalo <mphaloemmanuel@gmail.com>
2024-05-11 14:55:12 +0200

    #working code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Nested from "./Nested.svelte";
+    let textToDisplay = 'this is string containing <h1><i><strong>HTML!</strong></i></h1>';
+
+    let count = 0;
+    function increment() {
+        count += 1;
+    }
+
+    $: doubled = count * 2;
+
+</script>
+<h1>Welcome to Svelt</h1>
+<p>
+    Introduction page
+    {@html textToDisplay}
+</p>
+<p>{count} doubled is {doubled}</p>
+<button on:click={increment}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+</button>
+<Nested />
+
+<style>
+    h1 {
+        border-bottom: 0.5px solid blue;
+    }
+    button{
+        border: 1px solid black;
+        border-radius: 2px 2px;
+        background-color: gray;
+        font-size: large;
+        padding: 1em 2em;
+        color: white;
+    }
+</style>
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,11 @@
+<p>
+    This is a parapraph in another component
+</p>
+
+<style>
+    p {
+        border: 0.5px solid black;
+        padding: 3em;
+        background-color: aqua;
+    }
+</style>
